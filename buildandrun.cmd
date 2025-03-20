212e879c046e24fbc0fa41c6e55c49f926316720
Mario Cesar Silva Elvira <50765958+ExtraMobs@users.noreply.github.com>
2025-03-20 01:18:01 -0300

    include biblioteca io

---
+++ buildandrun.cmd
@@ -0,0 +1,4 @@
+@echo off
+cls
+gcc main.c -o main.exe
+main.exe
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <io.h>
+
+struct Carta
+{
+    char estado;
+    char codigo_carta[3];
+};
+
+
+int main(){
+    return 0;
+}
