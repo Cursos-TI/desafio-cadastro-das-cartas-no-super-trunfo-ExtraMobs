12e42bce5caa4f8f5bc5e56111cb428ae20a5b54
unknown <mariocesarelvira@hotmail.com>
2025-04-20 00:52:02 -0300

    Nível Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -38,8 +38,45 @@
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turisticos.
+    int populacao1;
+    float area1;
+    float pib1;
+    int pts_turisticos1;
+
+    int populacao2;
+    float area2;
+    float pib2;
+    int pts_turisticos2;
+
+
+    printf("Carta 1 - (int) Populacao: ");
+    scanf("%d", &populacao1);
+    printf("Carta 1 - (float) Area (km^2): ");
+    scanf("%f", &area1);
+    printf("Carta 1 - (float) PIB: ");
+    scanf("%f", &pib1);
+    printf("Carta 1 - (int) Pontos Turisticos: ");
+    scanf("%d", &pts_turisticos1);
+
+    printf("\nCarta 2 - (int) Populacao: ");
+    scanf("%d", &populacao2);
+    printf("Carta 2 - (float) Area (km^2): ");
+    scanf("%f", &area2);
+    printf("Carta 2 - (float) PIB: ");
+    scanf("%f", &pib2);
+    printf("Carta 2 - (int) Pontos Turisticos: ");
+    scanf("%d", &pts_turisticos2);
+
+    printf(
+        "Cidade 1:\n\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\n Pontos Turisticos: %d",
+        populacao1, area1, pib1, pts_turisticos1
+    );
+    printf(
+        "\n\nCidade 2:\n\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\n Pontos Turisticos: %d",
+        populacao2, area2, pib2, pts_turisticos2
+    );
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
