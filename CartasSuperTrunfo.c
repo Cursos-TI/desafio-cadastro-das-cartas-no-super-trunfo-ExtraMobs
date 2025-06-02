8f36595add2997ccccab1db94b69233f7fc13d12
unknown <mariocesarelvira@hotmail.com>
2025-06-01 23:22:08 -0300

    TypeDefs iniciais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,12 @@
 // estrutura exigida pelo enunciado, afinal a 
 // linguagem C não possui conceito de herança.
 
-// typedef struct
-// {
-//     char estado; // A - H
-//     int cidade; // 01 - 04
-//     char codigo_carta[3]; // estado + cidade
-// } CartaId;
+typedef struct
+{
+    char estado; // A - H
+    int cidade; // 01 - 04
+    char codigo_carta[3]; // estado + cidade
+} CartaId;
 
 // Pelo o que pesquisei, esse é o jeito
 // mais utilizado pela comunidade para representar
@@ -20,14 +20,14 @@
 // código escrito, podendo "construir" a estrutura,
 // parecido com as linguagens de mais alto nível.
 
-// typedef struct
-// {
-//     CartaId id;
-//     int populacao;
-//     float area;
-//     float pib;
-//     int pontos_turisticos;
-// } CartasSuperTrunfo;
+typedef struct
+{
+    CartaId id;
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+} CartasSuperTrunfo;
 
 
 // Desafio Super Trunfo - Países
@@ -37,53 +37,6 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turisticos.
-    int populacao1;
-    float area1;
-    float pib1;
-    int pts_turisticos1;
-
-    int populacao2;
-    float area2;
-    float pib2;
-    int pts_turisticos2;
-
-
-    printf("Carta 1 - (int) Populacao: ");
-    scanf("%d", &populacao1);
-    printf("Carta 1 - (float) Area (km^2): ");
-    scanf("%f", &area1);
-    printf("Carta 1 - (float) PIB: ");
-    scanf("%f", &pib1);
-    printf("Carta 1 - (int) Pontos Turisticos: ");
-    scanf("%d", &pts_turisticos1);
-
-    printf("\nCarta 2 - (int) Populacao: ");
-    scanf("%d", &populacao2);
-    printf("Carta 2 - (float) Area (km^2): ");
-    scanf("%f", &area2);
-    printf("Carta 2 - (float) PIB: ");
-    scanf("%f", &pib2);
-    printf("Carta 2 - (int) Pontos Turisticos: ");
-    scanf("%d", &pts_turisticos2);
-
-    printf(
-        "Cidade 1:\n\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\n Pontos Turisticos: %d",
-        populacao1, area1, pib1, pts_turisticos1
-    );
-    printf(
-        "\n\nCidade 2:\n\nPopulacao: %d\nArea: %.2f\nPIB: %.2f\n Pontos Turisticos: %d",
-        populacao2, area2, pib2, pts_turisticos2
-    );
-
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
 }
